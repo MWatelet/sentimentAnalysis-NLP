3b32c1f207191963aca1d8fc5f9053d7d27964e3
Watelet Matéo <mateo.watelet@gmail.com>
2024-11-24 19:25:08 +0100

    just an endpoint for api calls

--- app/main.py
+++ app/main.py
@@ -27,5 +27,13 @@ def review_page():
         sentiments = analyze_sentiment_with_models(review)
     return render_template("index.html", sentiments=sentiments)
 
+
+@app.route("get_sentiment", methods=['GET'])
+def get_sentiment():
+    review = request.json['review']
+    sentiments = analyze_sentiment_with_models(review)
+    return {"sentiments": sentiments}
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
